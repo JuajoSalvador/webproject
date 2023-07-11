0d9e009824290cf2f9c157acf4e4d9da67aef4b6
{JuajoSalvador} <{juanjo.salvador@mtp.es}>
2023-07-11 22:33:42 +0200

    110720236

--- webproject/settings.py
+++ webproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1#gsbyr=fqp5=gx080n5e7qgq6f3%*u&b2bkj!(box5hrg)z7&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
